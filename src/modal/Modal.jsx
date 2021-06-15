0af8ced6ddefe07f5fc6215eaa45e23c220a00e9
Mateusz Malec <m.mateusz2000@wp.pl>
2021-06-15 14:33:03 +0200

    feat: add all styles for modal

--- src/modal/Modal.jsx
+++ src/modal/Modal.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
+import './Modal.scss';
+
 const Modal = () => {
     return (
-        <div className="modal">
-            <h3>edit task</h3>
+        <div className="modal modal--active">
+            <h3 className="modal__header">edit task</h3>
             <form>
-                <div>
-                    <input type="text" /> <button>edit</button>
+                <div className="modal__box">
+                    <input type="text" className="modal__box-input" />{' '}
+                    <button className="modal__btn">add</button>
                 </div>
-                <button>cancel</button>
+                <button className="modal__btn modal__btn--cancel">cancel</button>
             </form>
         </div>
     );
