5014b730d13baba351825c628c5bf459f831dfe3
Mateusz Malec <m.mateusz2000@wp.pl>
2021-06-16 11:24:35 +0200

    fix: fix modal position

--- src/addTask/AddTask.jsx
+++ src/addTask/AddTask.jsx
@@ -9,28 +9,28 @@ let ID = 3;
 const AddTask = () => {
     const { dispatch } = useContext(AppContext);
 
-    const [addTaskValue, setAddTaskValue] = useState('');
-    const [addTaskError, setAddTaskError] = useState(false);
+    const [taskValue, setTaskValue] = useState('');
+    const [taskError, setTaskError] = useState(false);
 
-    const handleAddTaskValue = event => setAddTaskValue(event.target.value);
+    const handleAddTaskValue = event => setTaskValue(event.target.value);
     const handleaddTaskSubmit = event => {
         event.preventDefault();
-        if (addTaskValue.length < 2) {
-            setAddTaskError(true);
+        if (taskValue.length < 2) {
+            setTaskError(true);
             return;
         }
 
         const newTask = {
             id: ID,
-            name: addTaskValue,
+            name: taskValue,
             done: false,
         };
 
         dispatch({ newTask, type: ADD });
 
         ID++;
-        setAddTaskValue('');
-        setAddTaskError(false);
+        setTaskValue('');
+        setTaskError(false);
     };
 
     return (
@@ -39,12 +39,12 @@ const AddTask = () => {
                 Add task
                 <input
                     onChange={handleAddTaskValue}
-                    value={addTaskValue}
+                    value={taskValue}
                     className="addTask__input"
                     type="text"
                 />
             </label>
-            {addTaskError && (
+            {taskError && (
                 <p className="addTask__error">The task must be at least 2 characters long</p>
             )}
             <button className="addTask__btn">add</button>
